2637e243c52b20e726f1b649eadf9aba7d0d4b34
Srivatsa Viswanadhula <srivatsaviswanadhula@Srivatsas-MacBook-Air.local>
2026-06-24 02:17:30 -0400

    new file

--- src/random.rs
+++ src/random.rs
@@ -1,14 +1,4 @@
-fn main() {
-    println!("Hello, mac");
-    let num = 10;
-    if num > 10{
-        print!("greater than 10")
-    }
-    else if num == 10{
-        print!("equal to 10")
-    }
-    else if num < 10{
-        print!("Less than 10")
-    }
-    println!("Collance")
+
+fn new_person() {
+    print!("jfioewj")
 }
